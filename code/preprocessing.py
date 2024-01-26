c812895bb26be468f237f9cece13a5b55c8a2e93
gunwoo <gunwoo45954@gmail.com>
2024-01-26 02:25:18 +0000

    0.5.3

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -11,9 +11,9 @@ def data_preprocess(data):
         DataFrame: 데이터 전처리를 수행하고 나서 DataFrame을 반환합니다.
     """
     
-    # 특수문자 제거
     for idx, text in enumerate(data["text"]):
-        preprocessed_data = re.sub('[^A-Za-zㄱ-힣0-9\s]', ' ', text)
+        preprocessed_data = re.sub('[^A-Za-zㄱ-힣0-9\s]', ' ', text)    # 특수문자 제거
+        preprocessed_data = re.sub('[一-龥]', '', preprocessed_data)    # 한자 제거
         data.iloc[idx, 1] = preprocessed_data
         
         
